3c1d48817bfa8b1dbe04862dba8daf95827c0e14
Dhrupad Bhardwaj <dhrupadb@seas.upenn.edu>
2014-01-30 15:08:11 -0500

    Update Movie.java

--- HW01/Database/Movie.java
+++ HW01/Database/Movie.java
@@ -1,40 +1,39 @@
 package Database;
 
-// Implementation of an Individual [Actor Actress etc.]
+//Implementation of a Movie
 
-public class Individual
+public class Movie
 {
-	private int year;
-	private String category;
+	private String name;
 	private boolean win;
 	
 	
-	public Individual() // Default constructor
+	public Movie() // Default constructor
 	{
-		year = 0000;
-		category = "";
+		name = "";
 		win = false;
 	}
 	
-	public Individual(String category, int year, boolean win)
+	public Movie(String name, boolean win)
 	{
-		this.year = year;
-		this.category = category;
+		this.name = name;
 		this.win = win;
 	}
-	
+
+	// As the Database only handles the Best Picture category. This is hard Coded.
+	// Easily modifiable for other categories as well.
 	public String getCategory() {
-		return category;
+		return "Best Picture";
 	}
 
 	
-	public int getYear() {
-		return year;
+	public String getName() {
+		return name;
 	}
 
 	
 	public boolean didTheyWin() {
 		return win;
 	}
-
+	
 }
